86c328540c4df6d40426b08cef7d03961aebc320
b068931 <Mihail@DESKTOP-A5BJTLH>
2023-10-06 13:42:44 +0300

    Fix brands images resizing on aboutus page

--- WebApp/wwwroot/js/aboutUs.js
+++ WebApp/wwwroot/js/aboutUs.js
@@ -2,11 +2,13 @@ $(function () {
     var boxSize = 155;
     $(".brand-preview-image").each(function () {
         if ($(this).prop("complete") === true) {
-            $(this).makeImageFitBox(boxSize, boxSize);
+            $(this).makeImageFitBox(boxSize, boxSize)
+                .removeClass("d-none");
         }
         else {
             $(this).on("load", function () {
-                $(this).makeImageFitBox(boxSize, boxSize);
+                $(this).makeImageFitBox(boxSize, boxSize)
+                    .removeClass("d-none");
             });
         }
     });
